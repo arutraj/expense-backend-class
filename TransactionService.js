7737e087da0bae82960c99bde58f8c50b5bca7d1
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-12 09:01:13 +0530

    App with DOCKER

--- TransactionService.js
+++ TransactionService.js
@@ -1,5 +1,5 @@
 const dbcreds = require('./DbConfig');
-const mysql = require('mysql');
+const mysql = require('mysql2'); // Change to mysql2
 
 const con = mysql.createConnection({
     host: process.env.DB_HOST || dbcreds.DB_HOST,
@@ -8,14 +8,22 @@ const con = mysql.createConnection({
     database: process.env.DB_DATABASE || dbcreds.DB_DATABASE
 });
 
-function addTransaction(amount,desc){
-    var mysql = `INSERT INTO \`transactions\` (\`amount\`, \`description\`) VALUES ('${amount}','${desc}')`;
-    con.query(mysql, function(err,result){
-        if (err) throw err;
-        //console.log("Adding to the table should have worked");
-    }) 
-    return 200;
+function addTransaction(amount, desc) {
+    if (!amount || isNaN(Number(amount))) {
+        console.error("Invalid or empty amount provided.");
+        return { error: "Invalid or empty amount provided." };
+    }
+    var sql = `INSERT INTO \`transactions\` (\`amount\`, \`description\`) VALUES ('${amount}', '${desc}')`;
+    con.query(sql, function(err, result) {
+        if (err) {
+            console.error("Error adding transaction:", err.message);
+            return { error: err.message };
+        }
+        console.log("Transaction added successfully.");
+    });
+    return 200; // Consider changing to proper async handling or callback pattern
 }
+
 
 function getAllTransactions(callback){
     var mysql = "SELECT * FROM transactions";
@@ -32,7 +40,7 @@ function findTransactionById(id,callback){
         if (err) throw err;
         console.log(`retrieving transactions with id ${id}`);
         return(callback(result));
-    }) 
+    })
 }
 
 function deleteAllTransactions(callback){
@@ -41,7 +49,7 @@ function deleteAllTransactions(callback){
         if (err) throw err;
         //console.log("Deleting all transactions...");
         return(callback(result));
-    }) 
+    })
 }
 
 function deleteTransactionById(id, callback){
@@ -50,15 +58,13 @@ function deleteTransactionById(id, callback){
         if (err) throw err;
         console.log(`Deleting transactions with id ${id}`);
         return(callback(result));
-    }) 
+    })
 }
 
-
-module.exports = {addTransaction ,getAllTransactions, deleteAllTransactions, deleteAllTransactions, findTransactionById, deleteTransactionById};
-
-
-
-
-
-
-
+module.exports = {
+    addTransaction,
+    getAllTransactions,
+    findTransactionById,
+    deleteAllTransactions,
+    deleteTransactionById
+};
